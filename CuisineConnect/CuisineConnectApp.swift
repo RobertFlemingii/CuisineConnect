8211e479130d12b3b0f4032e61f36f20c45bc0e5
Robert Fleming <robertefleming.ca@gmail.com>
2023-08-21 16:22:56 -0700

    Initial Commit

---
+++ CuisineConnect/CuisineConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuisineConnectApp.swift
+//  CuisineConnect
+//
+//  Created by Robert Edward Fleming on 8/21/23.
+//
+
+import SwiftUI
+
+@main
+struct CuisineConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
